aa6086b590952ad1920d5e5e829db474b0f919af
ahmedthinkdifferent <ahmed.thinkdifferent@gmail.com>
2022-01-01 14:20:36 +0200

    pre push hook

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
@@ -7,9 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    var name = 'ahmed';
     //1- test
-
     return this.appService.getHello();
   }
 }
